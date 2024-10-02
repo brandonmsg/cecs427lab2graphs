fbc1de2153c52265c1b28f5d690d5f0d555e2741
brandonmsg <152921652+brandonmsg@users.noreply.github.com>
2024-10-02 14:56:27 -0700

    added graphs given, and adjusted arguments accordingly

--- graph_analysis.py
+++ graph_analysis.py
@@ -14,8 +14,7 @@ def plot_graph():
     return 0
 
 def clustering_coefficient(graph):
-    cluster = nx.clustering(graph)
-    return cluster
+    return nx.clustering(graph)
 
 def neighborhood_overlap(graph):
 
@@ -50,33 +49,44 @@ def plot_graph(graph, initial_node):
 
     plt.show()
 """
-
-def main():
+def parse_args(): # input arguments & check validation
     parser = ap.ArgumentParser(description='Creates graph based on Erdős–Rényi theorem and BFS algorithm')
 
-    parser.add_argument('--input', type=str, help='Filename of graph, ex. "out_graph_file.gml"')
-    parser.add_argument('--create_random_graph', action='store_true')
-    parser.add_argument('--nodes', type=int, help='Number of nodes for your graph')
-    parser.add_argument('--constant', type=float)
-    #parser.add_argument('--a', type=int, help='a')
-    parser.add_argument('--plot', action='store_true')
-    parser.add_argument('--output', type=str, help='Filename for new graph, ex. "out_graph_file.gml"')
+    parser.add_argument('--file', choices=['homophily.gml','imbalanced_graph.gml','balanced_graph.gml'], help='Choose which graph you would like to use: homophily.gml, imbalanced_graph.gml, or balanced_graph.gml')
+    parser.add_argument('--components', type=int, help='Amount of componants, used to partition graph')
+    parser.add_argument('--plot', choices=['C','N','P'], help='Which plot you would like: cluster coefficient = C, neighborhood overlap = N, partition = P')
+    parser.add_argument('--verify_homophily', action='store_true', help='Checks homophily of graph, returns True or False')
+    parser.add_argument('--verify_balanced_graph', action='store_true' help='Checks balance of graph, returns True or False')
+    parser.add_argument('--output', type=str, help='Filename for new plotted graph, ex. "out_graph_file.gml"')
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
 
     graph = None
-
-    if args.create_random_graph:
-        if args.nodes and args.constant:
-            graph = create_random_graph(args.nodes, args.constant)
-    elif args.input:
-        graph = read_graph(args.input)
 
     #if args.BFS is not None and graph:
     #    breadth_first_search(graph, args.BFS)
 
     if args.plot and graph:
-        plot_graph(graph, args.BFS)
+        if args.plot == 'C':
+            graph = plot_graph(graph)
+        if args.plot == 'N':
+            graph = plot_graph(graph)
+        if args.plot == 'P':
+            graph = plot_graph(graph)
+
+    if args.components and args.plot == 'P':
+        partition_graph(graph, args.components)
+    elif args.components:
+        pass
+
+    if args.verify_homophily and graph:
+        verify_homophily(graph)
+
+    if args.verify_balanced_graph and graph:
+        verify_balanced_graph(graph)
 
     if args.output and graph:
         write_graph(graph, args.output)
