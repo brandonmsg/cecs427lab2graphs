6ab6ea121d2e8ec766bce90cae1f0b3570df610a
brandonmsg <152921652+brandonmsg@users.noreply.github.com>
2024-10-02 19:06:10 -0700

    more work done 10/2
    finished main, finished balanced graph function, need to finish other functions

--- graph_analysis.py
+++ graph_analysis.py
@@ -9,48 +9,35 @@ def read_graph(filename):
 def write_graph(graph, file):
     nx.write_gml(graph, file)
 
-def plot_graph():
-
-    return 0
+def plot_graph(graph):
+    plt.show(graph)
 
 def clustering_coefficient(graph):
     return nx.clustering(graph)
 
 def neighborhood_overlap(graph):
 
-    return 0
+    pass
 
-def partition_graph(graph, nodes):
+def partition_graph(graph, components):
 
-    return 0 
+    pass 
 
 def verify_homophily(graph):
 
-    return 0
+    pass
 
 def verify_balanced_graph(graph):
+    for triangle in nx.cycle_basis(graph): # networkx function, cycles through different groups of nodes
+        if len(triangle) == 3: # ensures 3 edges to make up a triangle
+            x,y,z = triangle # each node that makes up triangle
+            sign = (graph[x][y]['sign'] * graph[y][z]['sign'] * graph[x][z]['sign']) # takes edges and determines if possitive or negative
+            if sign < 0: # if negative, then graph is not balanced, returns False, if possitive, return True
+                return False
+    return True
 
-    return 0
-
-"""
-def plot_graph(graph, initial_node):
-    # Compute BFS tree
-    bfs_tree = nx.bfs_tree(graph, initial_node)
-
-    # Generate a layout using bfs_tree
-    pos = nx.bfs_layout(bfs_tree, start=initial_node)
-
-    # Generate a list of edges from BFS traversal
-    bfs_edges = list(nx.bfs_edges(graph, initial_node))
-
-    # Draw the graph
-    nx.draw(graph, pos=pos, with_labels=True, node_color='lightblue', node_size=500, font_size=10, font_color='black')
-    nx.draw_networkx_edges(graph, pos=pos, edgelist=bfs_edges, edge_color='red')
-
-    plt.show()
-"""
 def parse_args(): # input arguments & check validation
-    parser = ap.ArgumentParser(description='Creates graph based on Erdős–Rényi theorem and BFS algorithm')
+    parser = ap.ArgumentParser(description='Plots graph based on what parameters you put in, i.e. the graph you input and C N P, also verifies balnce and/or homophily of the graph')
 
     parser.add_argument('--file', choices=['homophily.gml','imbalanced_graph.gml','balanced_graph.gml'], help='Choose which graph you would like to use: homophily.gml, imbalanced_graph.gml, or balanced_graph.gml')
     parser.add_argument('--components', type=int, help='Amount of componants, used to partition graph')
@@ -66,24 +53,39 @@ def main():
 
     graph = None
 
-    #if args.BFS is not None and graph:
-    #    breadth_first_search(graph, args.BFS)
+    if args.file:
+        if args.file == 'homophily.gml':
+            graph = 'homophily.gml'
+        if args.file == 'imbalanced_graph.gml':
+            graph = 'imbalanced_graph.gml'
+        if args.file == 'balanced_graph.gml':
+            graph = 'balanced_graph.gml'
+
+    if args.components:
+        pass
 
     if args.plot and graph:
         if args.plot == 'C':
-            graph = plot_graph(graph)
+            graph = clustering_coefficient(graph)
+            plot_graph(graph)
         if args.plot == 'N':
-            graph = plot_graph(graph)
-        if args.plot == 'P':
-            graph = plot_graph(graph)
+            graph = neighborhood_overlap(graph)
+            plot_graph(graph)
+        if args.plot == 'P' and args.components:
+            graph = partition_graph(graph, args.components)
+            plot_graph(graph)
+        elif args.plot == 'P' and not args.components:
+            print('Please enter number of components if you wanna partition the graph.')
+            print('Try again.')
+            exit
 
-    if args.components and args.plot == 'P':
-        partition_graph(graph, args.components)
-    elif args.components:
-        pass
-
-    if args.verify_homophily and graph:
+    if args.verify_homophily and graph == 'homophily.gml':
         verify_homophily(graph)
+
+    elif args.verify_homophily and graph != 'homophily.gml':
+        print('Please use "homphily.gml" as the graph input you want to use to verify homophily')
+        print('Try again.')
+        exit
 
     if args.verify_balanced_graph and graph:
         verify_balanced_graph(graph)
